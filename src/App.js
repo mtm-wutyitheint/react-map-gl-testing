762ff4cbf05b25b5874f335e16764189bde3e283
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-12-08 18:14:19 +0630

    add marker

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
-import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+import * as parkDate from "./data/skateboard-parks.json";
+// import { Marker } from 'react-map-gl';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
@@ -11,10 +13,8 @@ let lon = 96.1346011283,
   lat = 16.8246550347;
 
 class App extends React.Component {
-  
 
   componentDidMount() {
-
     // Creates new map instance
     const map = new mapboxgl.Map({
       container: this.mapWrapper,
@@ -31,13 +31,29 @@ class App extends React.Component {
       profile: 'mapbox/driving',
     });
 
+    for (const feature of parkDate.features) {
+      // create a HTML element for each feature
+      const el = document.createElement('div');
+      el.className = 'marker';
+       
+      // make a marker for each feature and add it to the map
+      new mapboxgl.Marker(el)
+      .setLngLat(feature.geometry.coordinates)
+      .setPopup(
+      new mapboxgl.Popup({ offset: 25 }) // add popups
+      .setHTML(
+      `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+      )
+      )
+      .addTo(map);
+      }
+
     // Integrates directions control with map
     map.addControl(directions, 'top-left');
   }
 
   render() {
     return (
-      // Populates map by referencing map's container property
       <div ref={el => (this.mapWrapper = el)} className="mapWrapper" />
     );
   }
